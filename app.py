d755a897c228580a9d14d128ea72921352380390
michael wu <michael10366@outlook.com>
2021-07-07 19:52:58 +0800

    last version

--- app.py
+++ app.py
@@ -42,11 +42,12 @@ def handle_message(event):
 
     if '貼圖' in msg:
         sticker_message = StickerSendMessage(
-    package_id='1',
-    sticker_id='1')
+        package_id='1',
+        sticker_id='1')
         line_bot_api.reply_message(
         event.reply_token,
         sticker_message)
+        return
 
 
 
